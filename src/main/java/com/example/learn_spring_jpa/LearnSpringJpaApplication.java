0fac338113888482f1430f274159af781f5f1092
portLee <hungu32@gmail.com>
2024-01-23 19:57:50 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/learn_spring_jpa/LearnSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.learn_spring_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringJpaApplication.class, args);
+	}
+
+}
